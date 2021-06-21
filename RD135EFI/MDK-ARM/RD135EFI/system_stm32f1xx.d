e69a0e62a918891909c2881dc1e5470eefa67b69
fjerena <fabio_jerena@yahoo.com.br>
2021-06-21 23:45:56 +0200

    It was configured PWM port, fix the lambda enrichment routine, this version is the most complete until this moment, but need a lot of validations

--- RD135EFI/MDK-ARM/RD135EFI/system_stm32f1xx.d
+++ RD135EFI/MDK-ARM/RD135EFI/system_stm32f1xx.d
@@ -12,6 +12,7 @@ rd135efi\system_stm32f1xx.o: ../Inc/stm32f1xx_hal_conf.h
 rd135efi\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
 rd135efi\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
 rd135efi\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+rd135efi\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
 rd135efi\system_stm32f1xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 rd135efi\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 rd135efi\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
